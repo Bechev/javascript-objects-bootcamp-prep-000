5896acc19720845a447bb79984fb169f0517cecb
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 19:53:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist (playlist, artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  delete playlist.artistName
+  playlist.delete [artistName]
   return playlist
 }
