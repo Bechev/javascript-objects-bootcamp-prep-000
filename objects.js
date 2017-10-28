0ed2cbe41d6609546188a08d1349c26ebb097ae3
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 19:37:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   ACDC: 'Thunderstruck'
 }
 
-updatePlaylist (obj, artistName,songTitle){
+function updatePlaylist (obj, artistName,songTitle){
   return Object.assign({},playlist,{artistName: songTitle})
 
 }
