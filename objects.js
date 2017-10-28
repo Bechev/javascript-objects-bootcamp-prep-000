cb9996eb8d40e2575b2e737786050784fc41afc6
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2017-10-28 20:00:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist (playlist, artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  delete playlist."Kayne";
+  delete playlist.artistName;
   return playlist;
 }
